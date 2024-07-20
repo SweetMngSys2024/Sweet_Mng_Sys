8ba994a9cb6a361211743c4738cd7bfa2cefb32c
BayanFutyan <s12112220@stu.najah.edu>
2024-07-20 21:53:39 +0300

    config

---
+++ sweet/src/test/java/testDefinitions/AcceptanceTest.java
@@ -0,0 +1,19 @@
+package testDefinitions;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "myfeatures", 
+    plugin = {"summary", "html:target/cucumber/report.html"},
+    monochrome = true,
+    snippets = SnippetType.CAMELCASE,				
+    glue = "testDefinitions" )
+
+
+public class AcceptanceTest {
+
+}
