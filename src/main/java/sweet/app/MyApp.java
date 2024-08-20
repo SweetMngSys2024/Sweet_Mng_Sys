6e900a00512967857ebd24a8af5f0651f206aa5f
Shams Abd Al-Aziz <s12113658@stu.najah.edu>
2024-08-20 23:36:29 +0300

    Update MyApp.java

--- src/main/java/sweet/app/MyApp.java
+++ src/main/java/sweet/app/MyApp.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class MyApp {
 	 private static final Logger logger = Logger.getLogger(MyApp.class.getName());
 	 
